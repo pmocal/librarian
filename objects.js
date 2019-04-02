67024d45459c7898d0061da4f3c2ac974bf07818
Parthiv Mohan <parthiv.mohan@gmail.com>
2019-04-02 16:57:05 -0400

    properly associated html with javascript objects

--- objects.js
+++ objects.js
@@ -1,17 +1,20 @@
 let myLibrary = []
 
 function Book(title, author, numpages, read) {
+  
   // the constructor...
   this.title = title;
   this.author = author;
   this.numpages = numpages;
   this.read = read;
+
 }
 
 function addBookToLibrary(book, library) {
-  // do stuff here
+
   library.push(book);
   return library;
+
 }
 
 function toggleReadStatus(library, index) {
@@ -19,10 +22,14 @@ function toggleReadStatus(library, index) {
 }
 
 function render(object) {
+
 	$("#myLibrary").remove();
+
 	var div = document.createElement("div");
 	div.setAttribute('id', "myLibrary");
+
 	for (index in object) {
+		
 		var line = document.createElement("div");
 		line.setAttribute('id', index);
 		line.innerHTML = object[index].title + ", " + object[index].author + ", " + object[index].numpages;
@@ -32,28 +39,39 @@ function render(object) {
 		if (object[index].read == true) {
 			readStatus.setAttribute("checked", true);
 		}
+		readStatus.addEventListener('click', function() {
+			myLibrary[index].read = !myLibrary[index].read;
+			render(myLibrary);
+		}, false);
 		line.appendChild(readStatus);
 
 		var removal = document.createElement("button");
 		removal.innerHTML = "Remove";
 		removal.addEventListener('click', function() {
 			document.getElementById(index).remove();
+			myLibrary.pop(index);
+			render(myLibrary);
 		}, false);
 		line.appendChild(removal);
 		
 		div.appendChild(line);
 	}
 	document.body.appendChild(div);
+
 };
 
 $(".newbook").click(function() {
+
 	$("form").show();
+
 });
 
 $(".submitbook").click(function() {
+
 	console.log(document.getElementById("read").getAttribute("checked"));
 	var book = new Book($("#author").val(), $("#title").val(), $("#numpages").val(), document.getElementById("read").checked);
 	addBookToLibrary(book, myLibrary);
 	render(myLibrary);
 	$("form").hide();
+
 });
