429980abb058f6fb0bdbb0df9677f2924b0a8027
Parthiv Mohan <parthiv.mohan@gmail.com>
2019-04-02 16:30:49 -0400

    fixed heroku deployment by serving all files

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var app = express();
 
-var path = require('path');
+app.use(express.static(__dirname));
 
-// viewed at http://localhost:8080
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile('index.html');
 });
 
 app.listen((process.env.PORT || 8080), function () {
